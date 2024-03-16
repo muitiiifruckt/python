3045fb2ad64e6f2e4392226d9727bdf6f0d7ffaa
ayzat <aayzat2004@gmail.com>
2024-03-16 17:59:00 +0300

    3 commit / make normal structure and decomposition of  js / css / html

--- chess/app/views.py
+++ chess/app/views.py
@@ -5,24 +5,12 @@ from .game_logic import ChessLogic
 
 
 # Предполагается, что используется AJAX для передачи хода
-def make_move(request):
-    if request.method == 'POST':
-        move = request.POST.get('move')
-        game_id = request.POST.get('game_id')
-        # Здесь должна быть логика для поиска соответствующей игры
-
-        chess_logic = ChessLogic()  # Создать новый экземпляр логики шахмат
-        if chess_logic.make_move(move):
-            return JsonResponse({'status': 'success'})
-        else:
-            return JsonResponse({'status': 'error', 'message': 'Invalid move'})
-    return JsonResponse({'status': 'error', 'message': 'Invalid request'})
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "html/index.html")
 def game(request):
-    return render(request, "game.html")
+    return render(request, "html/game.html")
 
 def about(request):
     return HttpResponse("<h2>Этот сайт является курсовой работой Муртазина Айзата Радиковича 09-141</h2>")
--- chess/chess/urls.py
+++ chess/chess/urls.py
@@ -1,22 +1,6 @@
-"""
-URL configuration for chess project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from . import views
+from app import views
 
 urlpatterns = [
     path('',views.index,name = "home"),
@@ -24,5 +8,4 @@ urlpatterns = [
     path('about', views.about),
     path('contact', views.contact),
     path('news', views.news),
-    path('make_move/', views.make_move,name='make_move'),
 ]
